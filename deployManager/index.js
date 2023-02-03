d91804bb87feac897a45bf5849e7b85cf5d318f0
tayyab javed <tayyabdev@outlook.com>
2023-02-03 11:49:03 +0000

    deploy manager

---
+++ deployManager/index.js
@@ -0,0 +1,7 @@
+//code should get events from github and build & deploy the scrapper
+//as well as setup any events in the eventbridge the scheduling
+exports.handler = async (event, opt) => {
+    return {
+        msg:"test"
+    }
+};
